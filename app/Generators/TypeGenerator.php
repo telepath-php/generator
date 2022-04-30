b1c9fbb4ad49f1947a39bc8da92f184ef41a660d
Tii <mail@tii.one>
2022-05-01 00:42:00 +0200

    Add a static make method with parameters

--- app/Generators/TypeGenerator.php
+++ app/Generators/TypeGenerator.php
@@ -18,22 +18,37 @@ class TypeGenerator
         $class->setExtends($type->extends);
         $class->addComment($type->description);
 
+        $makeMethod = $class->addMethod('make')
+            ->setStatic()
+            ->setReturnType('static');
+
+        $makeMethod->addBody('return new static([');
+
         foreach ($type->fields as $field) {
 
-            $property = $class->addProperty($field->name);
-            $property->setType($field->phpType);
-            $property->setPublic();
-            $property->addComment($field->description);
+            $property = $class->addProperty($field->name)
+                ->setType($field->phpType)
+                ->addComment($field->description);
+
+            $makeMethod->addComment('@param ' . $namespace->simplifyType($field->phpDocType ?? $field->phpType)
+                . ' $' . $field->name . ' ' . $field->description);
+            $parameter = $makeMethod->addParameter($field->name)
+                ->setType($field->phpType);
 
             if ($field->optional) {
-                $property->setNullable();
+                $property->setNullable()->setInitialized();
+                $parameter->setNullable()->setDefaultValue(null);
             }
 
             if ($field->phpDocType !== null) {
                 $property->addComment('@var ' . $namespace->simplifyType($field->phpDocType));
             }
 
+            $makeMethod->addBody("    ? => \${$field->name},", [$field->name]);
+
         }
+
+        $makeMethod->addBody(']);');
 
         return (string) $file;
     }
